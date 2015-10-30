e0c5fff28cc0f1a3e0f93f02bdf55951a7f0c732
Artomiron <redtom93@gmail.com>
2015-10-30 17:02:28 +0200

    RAM testing

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,14 +10,61 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            int x = 1;
+            Func1( x );
+            Console.WriteLine("Call Func1. x = " + x);
 
+            x = 1;
+            Func2(x);
+            Console.WriteLine("Call Func2. x = " + x);
+
+            x = 1;
+            Func3(x);
+            Console.WriteLine("Call Func3. x = " + x);
 
             Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Move(p1, 10, 10);
+            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y);
 
             Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            p1 = p2;
+            p2.x = 8;
+            p2.y = 8;
+            Console.WriteLine("p1 = p2. p1.x = " + p1.x + ", p1.y = " + p1.y + ", p2.x = " + p2.x + ", p2.y = " + p2.y);
 
+            p1 = new Point(1, 3, '*');
+            Reset(p1);
+            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y);
+
+            Console.ReadLine();
+        }
+
+        public static void Func1(int value)
+        {
+
+        }
+
+        public static void Func2 (int value)
+        {
+            value = value + 1;
+        }
+
+        public static void Func3 (int x)
+        {
+            x = x +1;
+        }
+
+        public static void Move(Point p, int dx, int dy)
+        {
+            p.x = p.x + dx;
+            p.y = p.y + dy;
+        }
+
+        public static void Reset(Point p)
+        {
+            p = new Point();
+        }
+      
             #region представление точек без ООП
             //Console.WriteLine("Let's start programing Snake!!!");
             /*int x1 = 1;
@@ -29,8 +76,5 @@ namespace Snake
             char sym2 = '*';
             Draw(x2, y2, sym2);*/
             #endregion
-
-            Console.ReadLine();
-        }
     }
 }
